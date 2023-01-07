8983275f9a23f98d396b682848a131e4fb7b25b0
Michal Zúbek <misozet5@gmail.com>
2023-01-07 15:06:25 +0100

    f

--- MenuObjectType.java
+++ MenuObjectType.java
@@ -6,29 +6,32 @@
  * @version (version number or date here)
  */
 public enum MenuObjectType {
-    LOADGAME("load picture"),
-    NEWGAME("new pic"),
-    EXITGAME("exit pic"),
-    ATTACK("attack picture"),
-    GUARD("guard pic"),
-    ELEMENTALHIT("elehit pic"),
-    ELEMENTALEFFECT("elefect picture"),
-    ITEMS("item pic"),
-    SMALLHP("smalhp picture"),
-    LARGEHP("larhp pic"),
-    SMALLMP("smana pic"),
-    LARGEMP("lmana picture"),
-    CONTINUE("conti pic"),
-    SAVEGAME("save pic"),
-    BACKTOMAINMENU("backtomm picture");
+    LOADGAME("loadGameUnselected.png","loadGameSelected.png"),
+    NEWGAME("newGameUnselected.png","newGameSelected.png"),
+    EXITGAME("exitGameUnselected.png","exitGameSelected.png"),
+    ATTACK("attackUnselected.png","attackSelected.png"),
+    GUARD("guardUnselected.png","guardSelected.png"),
+    ELEMENTALHIT("elementalHitUnselected.png","elementalHitSelected.png"),
+    ELEMENTALEFFECT("elementalEffectUnselected.png","elementalEffectSelected.png"),
+    ITEMS("itemsUnselected.png","itemsSelected.png"),
+    SMALLHP("smallHPUnselected.png","smallHPSelected.png"),
+    LARGEHP("largeHPUnselected.png","largeHPSelected.png"),
+    SMALLMP("smallMPUnselected.png","smallMPSelected.png"),
+    LARGEMP("largeMPUnselected.png","largeMPSelected.png"),
+    CONTINUE("continueUnselected.png","continueSelected.png"),
+    SAVEGAME("saveGameUnselected.png","saveGameSelected.png"),
+    BACKTOMAINMENU("backToMainMenuUnselected.png","backToMainMenuSelected.png");
     
-    private String picture;
+    private Obrazok unselectedPicture;
+    private Obrazok selectedPicture;
     
-    MenuObjectType(String picture) {
-        this.picture = picture;
+    MenuObjectType(String unselected, String selected) {
+        String filePath = 
+        this.unselectedPicture = new Obrazok(unselected);
+        this.selectedPicture = new Obrazok(selected);fffi
     }
     
-    public String getPicture() {
-        return this.picture;
+    public Obrazok getUnselectedPicture() {
+        return this.unselectedPicture;
     }
 }
